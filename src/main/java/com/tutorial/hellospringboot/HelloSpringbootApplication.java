3f4fccb1b70cc2f576956cd8b90354a79d72696f
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-21 21:42:50 +0700

    add Spring boot Devtools

--- src/main/java/com/tutorial/hellospringboot/HelloSpringbootApplication.java
+++ src/main/java/com/tutorial/hellospringboot/HelloSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Server listening ar http://localhost:8080");
 		SpringApplication.run(HelloSpringbootApplication.class, args);
 	}
 
